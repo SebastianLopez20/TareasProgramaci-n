e8d21a379a3c22eed55615446175c124aac07de2
SebastianLopez20 <64166116+SebastianLopez20@users.noreply.github.com>
2020-04-24 13:06:08 -0500

    Add files via upload
    Tarea tipos de datos

---
+++ TareaTres.java
@@ -0,0 +1,39 @@
+
+package tareatres;
+
+
+public class TareaTres {
+
+   
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "Sebastian"; 
+        char genero = 'm';
+        String civilStatus = "soltero";
+        String dateBirth = "30/08/1999";
+        int identificationCard = 1725321580;
+        char size = 's';
+        boolean ecuadorian = true;
+        String direction = "Turi el Pedregal/Cuenca";
+        int phone = 4151980;
+        String facebook = "Sebas Lopez";
+        double heigth = 1.71;
+        String favoriteSong = "Level of Concern";
+        boolean student = true;
+        
+        
+        System.out.println("Hola me llamo: "+name+ " tengo: "+age+" años "+" mi genero es "+genero+" mi estado civil es: "+civilStatus+" naci el "+dateBirth+" mi numnero de cedula es "+identificationCard+"");
+        System.out.println("soy de talla "+size+" mi altura es "+heigth+ " mi nacionalidad ecuatoriana es "+ecuadorian+" vivo en "+direction+" mi numero de casa es "+phone+" ");
+        System.out.println("me pueden encontrar en facebook como "+facebook+" mi cancion favorita es "+favoriteSong+" estudio en el sudamericano? "+student);
+        System.out.println("...............");
+        
+    
+        
+        
+        
+        
+        
+        
+    }
+    
+}
